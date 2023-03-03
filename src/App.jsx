a59c3376b4d016f9d06e46badcaab5cdc2e9f1cc
HITO <oumar2000mouhamed@gmail.com>
2023-03-04 00:50:57 +0300

    code enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,68 @@ import Nav from './components/nav';
 import './sass/index.scss';
 
 function App() {
+  function CreateBG({ name }) {
+    return (
+      <div className='bg-container'>
+        <picture>
+          <source
+            srcSet={require(`./assets/${name}/background-${name}-mobile.jpg`)}
+            media='(max-width: 411px)'
+          />
+          <source
+            srcSet={require(`./assets/${name}/background-${name}-tablet.jpg`)}
+            media='(max-width: 772px)'
+          />
+          <img
+            src={require(`./assets/${name}/background-${name}-desktop.jpg`)}
+            alt='home background'
+          />
+        </picture>
+      </div>
+    );
+  }
   return (
-    <div className='App'>
+    <div className='app'>
       <Nav />
 
       <Routes>
-        <Route path='/' index element={<Home />} />
-        <Route path='/destination' index element={<h1> destination </h1>} />
-        <Route path='/crew' index element={<h1> Crew </h1>} />
-        <Route path='/technology' index element={<h1> technology </h1>} />
+        <Route
+          path='/'
+          index
+          element={
+            <>
+              <CreateBG name='home' />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path='/destination'
+          element={
+            <>
+              <CreateBG name='destination' />
+              <h1> destination </h1>
+            </>
+          }
+        />
+        <Route
+          path='/crew'
+          element={
+            <>
+              <CreateBG name='crew' />
+              <h1> crew </h1>
+            </>
+          }
+        />
+        <Route
+          path='/technology'
+          element={
+            <>
+              <CreateBG name='technology' />
+              <h1> technology </h1>
+            </>
+          }
+        />
       </Routes>
     </div>
   );
