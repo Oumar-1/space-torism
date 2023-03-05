a23539f0ece3d64465147d54c57bfc0694148456
HITO <oumar2000mouhamed@gmail.com>
2023-03-05 03:46:48 +0300

    make logo a link to home

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/shared/logo.svg';
 import { ReactComponent as Hamburger } from '../assets/shared/icon-hamburger.svg';
 import { ReactComponent as Close } from '../assets/shared/icon-close.svg';
@@ -11,14 +11,16 @@ function Nav(props) {
   const { width: windowWidth } = useWindowSize();
   const [isNavOpen, setIsNavOpen] = useToggler(false);
   let delay = 0;
-  const diff = 150;
+  const diff = 100;
 
   const style = () => ({ transitionDelay: (delay += diff) + 'ms' });
   return (
     <nav>
-      <div className='logo'>
-        <Logo />
-      </div>
+      <Link to='/'>
+        <div className='logo'>
+          <Logo />
+        </div>
+      </Link>
       <hr />
       <ul className={`nav-links ${isNavOpen ? 'active' : ''} `}>
         <li style={style()}>
